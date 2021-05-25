807d41958177954ca2a3f22b8262e919c37e2a94
mabaoyan <mabaoyan@jd.com>
2021-05-25 11:33:05 +0800

    videoCapture start

---
+++ MachineLearningBasedSystem/MachineLearningBasedSystem/MLCapture.swift
@@ -0,0 +1,61 @@
+//
+//  MLCapture.swift
+//  MachineLearningBasedSystem
+//
+//  Created by mabaoyan on 2021/5/24.
+//
+
+import Foundation
+import AVKit
+
+protocol CaptureImageDelegate: AnyObject {
+    func captureOutput(_ output : CVPixelBuffer)
+}
+
+
+class MLCaputure : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    weak var delegate: CaptureImageDelegate?
+    let caputureSession : AVCaptureSession
+    
+    static let shared = MLCaputure()
+    
+    override init() {
+        caputureSession = AVCaptureSession()
+        caputureSession.sessionPreset = AVCaptureSession.Preset.photo
+        super.init()
+    }
+    
+    func setupCaptureSession() -> AVCaptureSession {
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            fatalError("获取设备异常")
+        }
+        
+        guard let input = try? AVCaptureDeviceInput.init(device: captureDevice) else { fatalError("获取设备异常")}
+        caputureSession.addInput(input)
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue.init(label: "videoQueue"))
+        caputureSession.addOutput(dataOutput)
+        
+        caputureSession.startRunning()
+        return caputureSession
+    }
+    
+    func captureLayer() -> AVCaptureVideoPreviewLayer {
+        return AVCaptureVideoPreviewLayer(session: self.setupCaptureSession())
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+        guard let _ = pixelBuffer else {
+            return
+        }
+        
+        guard let _ = self.delegate else {
+            return
+        }
+        
+        self.delegate?.captureOutput(pixelBuffer!)
+    }
+}
--- MachineLearningBasedSystem/MachineLearningBasedSystem/ViewController.swift
+++ MachineLearningBasedSystem/MachineLearningBasedSystem/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CaptureImageDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let capture = MLCaputure.shared
+        
+        let layer = capture.captureLayer()
+        capture.delegate = self
+        layer.frame = view.frame;
+        view.layer.addSublayer(layer)
     }
 
-
+    
+    
+    func captureOutput(_ output: CVPixelBuffer) {
+        print("hello")
+    }
+    
 }
 
